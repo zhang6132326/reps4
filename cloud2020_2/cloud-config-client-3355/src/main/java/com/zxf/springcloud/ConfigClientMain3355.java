078616768ba8f8c1d37f186b76972c8546eea449
zhang6132326 <6132326@qq.com>
2020-12-19 20:53:14 +0800

    11121

--- cloud2020_2/cloud-config-client-3355/src/main/java/com/zxf/springcloud/ConfigClientMain3355.java
+++ cloud2020_2/cloud-config-client-3355/src/main/java/com/zxf/springcloud/ConfigClientMain3355.java
@@ -2,12 +2,12 @@ package com.zxf.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
-@EnableConfigServer
-public class ConfigCenterMain3344 {
+public class ConfigClientMain3355 {
     public static void main(String[] args) {
-        SpringApplication.run(ConfigCenterMain3344.class,args);
+        SpringApplication.run(ConfigClientMain3355.class,args);
     }
 }
---
+++ cloud2020_2/cloud-config-client-3355/src/main/java/com/zxf/springcloud/config/ConfigClientController.java
@@ -0,0 +1,19 @@
+package com.zxf.springcloud.config;
+
+import org.springframework.beans.factory.annotation.Value;
+
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RefreshScope   //动态刷新配置服务中心的内容。
+public class ConfigClientController {
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/configInfo")
+    public String getConfigInfo(){
+        return configInfo;
+    }
+}
